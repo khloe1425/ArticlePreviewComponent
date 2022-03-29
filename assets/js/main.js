6fe3c7539dd5c8c5b249da09376ce2601b115f33
vietmit37 <nthanhtung1440@gmail.com>
2022-03-29 21:23:44 +0700

    complete 100%

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+document.querySelector(".info__icon").addEventListener("click", function () {
+  document.querySelector(".tooltip").classList.toggle("tooltip-click");
+  document.querySelector(".info__icon").classList.toggle("info__icon--click");
+  document
+    .querySelector(".info__icon .fas")
+    .classList.toggle("icon__color--change");
+});
+document.querySelector(".info__icon2").addEventListener("click", function () {
+  document
+    .querySelector(".tooltip__mobile")
+    .classList.toggle("tooltip__mobile--click");
+});
