05b5a2f915b429ff67f57ba40e8e2036607b64a0
vietmit37 <nthanhtung1440@gmail.com>
2022-03-30 02:15:24 +0700

    desktop-first

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Event click tooltip screen desktop
 document.querySelector(".info__icon").addEventListener("click", function () {
   document.querySelector(".tooltip").classList.toggle("tooltip-click");
   document.querySelector(".info__icon").classList.toggle("info__icon--click");
@@ -5,6 +6,7 @@ document.querySelector(".info__icon").addEventListener("click", function () {
     .querySelector(".info__icon .fas")
     .classList.toggle("icon__color--change");
 });
+// Event click tooltip screen mobile
 document.querySelector(".info__icon2").addEventListener("click", function () {
   document
     .querySelector(".tooltip__mobile")
